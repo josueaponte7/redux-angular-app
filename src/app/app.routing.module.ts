f127b3222713954dcc7c32df611fedc46bb8d8c0
Josue Aponte <josueaponte7@gmail.com>
2022-04-20 18:49:36 +0200

    Logout

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
@@ -18,6 +19,7 @@ const routes: Routes = [
     path: 'posts',
     loadChildren: () =>
       import('./components/posts/posts.module').then((m) => m.PostsModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'auth',
